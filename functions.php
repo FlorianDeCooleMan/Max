be3bae0400b352050d90093cfe81000eab807267
FlorianDeCooleMan <1201604@student.roc-nijmegen.nl>
2025-10-08 12:11:01 +0200

    fixing main.css

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+function school_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    register_nav_menus(array(
+        'main-menu' => __('Hoofdmenu', 'schoolthema'),
+    ));
+}
+add_action('after_setup_theme', 'school_theme_setup');
+
+function school_widgets_init() {
+    register_sidebar(array(
+        'name'          => __('Sidebar', 'schoolthema'),
+        'id'            => 'sidebar-1',
+        'before_widget' => '<section class="widget">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ));
+}
+add_action('widgets_init', 'school_widgets_init');
+
+function school_enqueue_styles() {
+    wp_enqueue_style('fontawesome', get_template_directory_uri() . '/assets/css/fontawesome-all.min.css');
+    wp_enqueue_style('school-main', get_stylesheet_directory_uri() . '/assets/sass/main.css', array(), null);
+
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('browser', get_template_directory_uri() . '/assets/js/browser.min.js', array('jquery'), null, true);
+    wp_enqueue_script('breakpoints', get_template_directory_uri() . '/assets/js/breakpoints.min.js', array('jquery'), null, true);
+    wp_enqueue_script('util', get_template_directory_uri() . '/assets/js/util.js', array('jquery'), null, true);
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'school_enqueue_styles');
+
+?>
