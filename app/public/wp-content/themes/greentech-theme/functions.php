eefe739754f0c3256dc80f9b54949bf2046f3b08
FlorianDeCooleMan <1201604@student.roc-nijmegen.nl>
2025-11-16 16:15:48 +0100

    widget area

--- app/public/wp-content/themes/greentech-theme/functions.php
+++ app/public/wp-content/themes/greentech-theme/functions.php
@@ -28,4 +28,52 @@ function greentech_widgets_init() {
     ));
 }
 add_action('widgets_init', 'greentech_widgets_init');
+
+function greentech_register_sidebar() {
+    register_sidebar( array(
+        'name'          => 'Sidebar',
+        'id'            => 'sidebar-1',
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2>',
+        'after_title'   => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'greentech_register_sidebar' );
+function greentech_mini_posts_shortcode() {
+    ob_start();
+    $args = array(
+        'posts_per_page' => 4,
+        'post_status' => 'publish'
+    );
+    $recent_posts = new WP_Query($args);
+
+    if ($recent_posts->have_posts()):
+        echo '<div class="mini-posts">';
+        while ($recent_posts->have_posts()): $recent_posts->the_post(); ?>
+            <article class="mini-post">
+                <header>
+                    <div class="title">
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    </div>
+                    <div class="meta">
+                        <time class="published" datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
+                        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" class="author">
+                            <?php echo get_avatar(get_the_author_meta('ID'), 48); ?>
+                        </a>
+                    </div>
+                </header>
+                <?php if (has_post_thumbnail()): ?>
+                    <a href="<?php the_permalink(); ?>" class="image"><?php the_post_thumbnail('thumbnail'); ?></a>
+                <?php endif; ?>
+            </article>
+        <?php endwhile;
+        echo '</div>';
+        wp_reset_postdata();
+    endif;
+
+    return ob_get_clean();
+}
+add_shortcode('mini_posts', 'greentech_mini_posts_shortcode');
+
 ?>
--- app/public/wp-content/themes/greentech-theme/sidebar.php
+++ app/public/wp-content/themes/greentech-theme/sidebar.php
@@ -1,58 +1,34 @@
 <aside id="sidebar">
-    <section id="intro">
-        <header>
-            <h2>GreenTech Solutions</h2>
-            <p>Lorem ipsum</p>
-        </header>
-    </section>
-    <!-- Dynamische Mini Posts -->
-    <section>
-        <div class="mini-posts">
-            <?php
-            $args = array(
-                'posts_per_page' => 4,
-                'post_status' => 'publish'
-            );
-            $recent_posts = new WP_Query($args);
 
-            if ($recent_posts->have_posts()):
-                while ($recent_posts->have_posts()):
-                    $recent_posts->the_post(); ?>
-<article class="mini-post">
-    <header>
-        <div class="title">
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-        </div>
+    <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+        <?php dynamic_sidebar( 'sidebar-1' ); ?>
+    <?php else : ?>
+        <!-- fallback voor wanneer er nog geen widgets zijn -->
 
-        <div class="meta">
-            <time class="published"
-                datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
+        <!-- Intro sectie -->
+        <section id="intro">
+            <header>
+                <h2>GreenTech Solutions</h2>
+                <p>Lorem ipsum</p>
+            </header>
+        </section>
 
-            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" class="author">
-                <?php echo get_avatar(get_the_author_meta('ID'), 48); ?>
-            </a>
-        </div>
-    </header>
+        <!-- Mini-posts via shortcode -->
+        <section>
+            <?php echo do_shortcode('[mini_posts]'); ?>
+        </section>
 
-    <?php if (has_post_thumbnail()): ?>
-        <a href="<?php the_permalink(); ?>" class="image"><?php the_post_thumbnail('thumbnail'); ?></a>
+        <!-- About / Blurb -->
+        <section class="blurb">
+            <h2>About</h2>
+            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod
+                amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.
+            </p>
+            <ul class="actions">
+                <li><a href="#" class="button">Learn More</a></li>
+            </ul>
+        </section>
+
     <?php endif; ?>
-</article>
-
-                <?php endwhile;
-                wp_reset_postdata();
-            endif;
-            ?>
-            <section class="blurb">
-                <h2>About</h2>
-                <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod
-                    amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.
-                </p>
-                <ul class="actions">
-                    <li><a href="#" class="button">Learn More</a></li>
-                </ul>
-            </section>
-        </div>
-    </section>
 
 </aside>
